a7eae8f0f4959d1f6419cd21bd2fd98e0b0d10e4
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2025-02-02 10:02:06 -0600

    Updates

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
+const guildIds = process.env.GUILD_IDS.split(",");
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
@@ -29,17 +29,21 @@ const rest = new REST({ version: "10" }).setToken(token);
 (async () => {
   try {
     console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
+      `Started refreshing ${commands.length} application (/) commands for multiple guilds.`
     );
 
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
+    for (const guildId of guildIds) {
+      const trimmedGuildId = guildId.trim();
+      const data = await rest.put(
+        Routes.applicationGuildCommands(clientId, trimmedGuildId),
+        { body: commands }
+      );
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands for guild ${trimmedGuildId}.`
+      );
+    }
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
+    console.log("All commands reloaded successfully for all specified guilds.");
   } catch (error) {
     console.error(error);
   }
