9fce45a5723c40ed621c503de47e5148ee15ae41
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2025-02-04 17:02:34 -0600

    Revert last

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ client.once("ready", () => {
         type: ActivityType.Custom,
         name: "custom",
         state: "Playing with commands",
-        emoji: { name: "kittyJam", id: "1336470651862781953" },
       },
     ],
   });
